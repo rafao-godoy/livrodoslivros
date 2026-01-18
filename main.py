86834f69abde86500414ead932044c6a584418b7
Rafael Godoy <rafa.godoy85@gmail.com>
2026-01-18 16:26:34 -0300

    Preparação para deploy em nuvem

--- main.py
+++ main.py
@@ -1,7 +1,52 @@
 from fastapi import FastAPI
+from database import criar_tabela
+from pydantic import BaseModel
+from database import salvar_ideia
+from database import listar_ideias
+from fastapi import Request, Form
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
+
+
+templates = Jinja2Templates(directory="templates")
 
 app = FastAPI(title="Livro dos Livros")
 
-@app.get("/")
-def home():
-    return {"status": "ok", "mensagem": "Livro dos Livros rodando 📘"}
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+@app.on_event("startup")
+def startup():
+    criar_tabela()
+
+class Ideia(BaseModel):
+    titulo: str | None = None
+    conteudo: str
+    rotulos: str | None = None
+
+@app.get("/", response_class=HTMLResponse)
+def home(request: Request):
+    ideias = listar_ideias()
+    return templates.TemplateResponse(
+        "index.html",
+        {"request": request, "ideias": ideias}
+    )
+
+@app.post("/ideias/form")
+def criar_ideia_form(
+    titulo: str = Form(None),
+    conteudo: str = Form(...),
+    rotulos: str = Form(None)
+):
+    salvar_ideia(titulo, conteudo, rotulos)
+    return {"status": "ok"}
+
+
+@app.post("/ideias")
+def criar_ideia(ideia: Ideia):
+    salvar_ideia(ideia.titulo, ideia.conteudo, ideia.rotulos)
+    return {"status": "salvo"}
+
+@app.get("/ideias")
+def obter_ideias():
+    return listar_ideias()
