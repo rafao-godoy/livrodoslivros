cebf63e7d162c8cc2aee996accbd316016b8cfd9
Rafael Godoy <rafa.godoy85@gmail.com>
2026-01-20 21:00:21 -0300

    Alteração do endereço do banco

--- database.py
+++ database.py
@@ -2,7 +2,7 @@ import sqlite3
 import os
 import psycopg2
 
-DATABASE_URL = os.getenv("postgresql://livro_dos_livros_db_user:AREnw9CuQ65Yy5LeK7M5PX7qvSPz538f@dpg-d5o118f5r7bs73ciqto0-a.oregon-postgres.render.com/livro_dos_livros_db")
+DATABASE_URL = os.getenv("postgresql://livro_dos_livros_db_user:AREnw9CuQ65Yy5LeK7M5PX7qvSPz538f@dpg-d5o118f5r7bs73ciqto0-a/livro_dos_livros_db")
 
 #DB_NAME = "livro.db"
 
